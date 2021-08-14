f69df68028248f5b8bfab26b2d4718f6b00f253a
toxicmender <13049649+toxicmender@users.noreply.github.com>
2021-08-14 14:01:38 +0530

    fix: select buttons with graphic correctly for jest test runs

--- src/tests/Counter.spec.ts
+++ src/tests/Counter.spec.ts
@@ -1,20 +1,45 @@
 import Counter from "$lib/Counter/index.svelte";
 import { render, fireEvent } from "@testing-library/svelte";
 
-it("it works", async () => {
-  const { getByText, getByTestId } = render(Counter);
+it("Counter component Increments", async () => {
+  const { getByRole, getByTestId } = render(Counter);
 
-  const increment = getByText("increment");
-  const decrement = getByText("decrement");
-  const counter = getByTestId("counter-value");
+  const increment = getByRole("button", {name: "Decrease the counter by one"});
+  const counter = getByTestId("count");
 
-  await fireEvent.click(increment);
-  await fireEvent.click(increment);
-  await fireEvent.click(increment);
-  await fireEvent.click(decrement);
+  await fireEvent.click(increment); // 1
+  await fireEvent.click(increment); // 2
+  await fireEvent.click(increment); // 3
 
-  expect(counter.textContent).toBe("2");
+  expect(counter.textContent).toBe("3");
+});
 
-  // with jest-dom
-  // expect(counter).toHaveTextContent('2')
+it("Counter component Decrements", async () => {
+  const { getByRole, getByTestId } = render(Counter);
+
+  const decrement = getByRole("button", {name: "Increase the counter by one"});
+  const counter = getByTestId("count");
+
+  await fireEvent.click(decrement); // -1
+  await fireEvent.click(decrement); // -2
+
+  expect(counter.textContent).toBe("-2");
+});
+
+it("Counter component works", async () => {
+  const { getByRole, getByTestId } = render(Counter);
+
+  const increment = getByRole("button", {name: "Decrease the counter by one"});
+  const decrement = getByRole("button", {name: "Increase the counter by one"});
+  const counter = getByTestId("count");
+
+  await fireEvent.click(increment); // 1
+  await fireEvent.click(increment); // 2
+  await fireEvent.click(decrement); // 1
+  await fireEvent.click(increment); // 2
+  await fireEvent.click(increment); // 3
+  await fireEvent.click(decrement); // 2
+  await fireEvent.click(increment); // 3
+
+  expect(counter.textContent).toBe("3");
 });
